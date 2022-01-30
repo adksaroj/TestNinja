6f37b86d07303280e29f0bca5b16f53f87b2732f
Saroj Adhikari <saroj.io@outlook.com>
2022-01-31 00:12:56 +0530

    Nunit

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfTwoArguments()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+            //Assert
+            var result = math.Add(5,7);
+            //Act
+            Assert.That(result, Is.EqualTo(5 + 7));
+        }
+
+        [Test]
+        public void Max_WhenFirstArgumentIsGreater_ReturnsTheFirstArgument()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+            //Assert
+            var result = math.Max(7, 5);
+            //Act
+            Assert.That(result, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Max_WhenSecondArgumentIsGreater_ReturnsTheSecondArgument()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+            //Assert
+            var result = math.Max(5, 8);
+            //Act
+            Assert.That(result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Max_WhenBothArgsEqual_ReturnsTheSameArgument()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+            //Assert
+            var result = math.Max(3, 3);
+            //Act
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User{ IsAdmin = true });
+
+            //Asset
+            //Assert.IsTrue(result);
+            Assert.That(result, Is.True); //Supported by NUnit
+            //Assert.That(result == true); //Supported by NUnit
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            var user = new User { IsAdmin = false };
+            reservation.MadeBy = user;
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Asset
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            //Arrange
+            var reservation = new Reservation { MadeBy = new User()};
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //Asset
+            Assert.IsFalse(result);
+        }
+    }
+}
