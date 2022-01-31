d616faf7b93553121e75b81aee18cce65f8f6ce4
Saroj Adhikari <saroj.io@outlook.com>
2022-01-31 13:55:02 +0530

    Testing Return Type of methods

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_WhenIdIsZero_ShouldReturnNotFoundObject()
+        {
+            var result = _customerController.GetCustomer(0);
+
+            //Assert
+            Assert.That(result, Is.TypeOf<NotFound>()); //TypeOf - result is exactly of type NotFound
+        }
+
+        [Test]
+        public void GetCustomer_WhenIdIsNonZero_ShouldReturnOkObject()
+        {
+            var result = _customerController.GetCustomer(1);
+
+            //Assert
+            //Assert.That(result, Is.InstanceOf<Ok>()); //InstanceOf - result is of type Ok
+            // or Derivative(child object) of Ok
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
