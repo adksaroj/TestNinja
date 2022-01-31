01ae49c4b6166d86df331a3f3d7c45d7b098c183
Saroj Adhikari <saroj.io@outlook.com>
2022-01-31 22:06:04 +0530

    StackTests

---
+++ TestNinja.UnitTests/Fundamentals/StackTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        private Fundamentals.Stack<object> _stack;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            _stack = new Fundamentals.Stack<object>();
+        }
+
+        [Test]
+        public void Push_WhenObjIsNull_ThrowsArgumentNullException()
+        {
+            Assert.That(() => _stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_WhenObjIsNotNull_CountShouldBeOne()
+        {
+            _stack.Push(new object());
+            Assert.That(_stack.Count, Is.EqualTo(1));
+
+        }
+
+        [Test]
+        public void Pop_WhenCountIsZero_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Pop(), Throws.Exception.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Pop_WhenCountIsNonZero_ReturnsObjectAndDecrementsCount()
+        {
+            _stack.Push(new object());
+            var result = _stack.Pop();
+            Assert.That(result, Is.TypeOf<object>());
+            Assert.That(_stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Peek_WhenCountIsZero_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => _stack.Peek(), Throws.Exception.TypeOf<InvalidOperationException>());
+        }
+
+        [Test]
+        public void Peek_WhenCountIsNonZero_ReturnsObject()
+        {
+            _stack.Push(new object());
+            var result = _stack.Peek();
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<object>());
+        }
+    }
+}
