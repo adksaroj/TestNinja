1f641b2dd49f5974d66c67344707176a594c9065
Saroj Adhikari <saroj.io@outlook.com>
2022-01-31 13:03:12 +0530

    Testing methods with strings

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        private HtmlFormatter _htmlFormatter;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+            _htmlFormatter = new HtmlFormatter();
+        }
+
+        [Test]
+        [TestCase("hello")]
+        public void FormatAsBold_WhenCalled_ReturnsBoldString(string inputString)
+        {
+            //Act
+            var result = _htmlFormatter.FormatAsBold(inputString);
+
+            //Assert - Specific
+            Assert.That(result, Is.EqualTo($"<strong>{inputString}</strong>"));
+
+            //More general
+            //Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            //Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            //Assert.That(result, Does.Contain(inputString));
+        }
+    }
+}
