1eff3b1f1f623b06864dcb232ddda3b9383f82d9
Saroj Adhikari <saroj.adhikari@clorox.com>
2023-01-29 19:58:47 +0530

    Add ProductTests

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_IsGoldCustomer_Apply30PercentDiscount()
+        {
+            var customer = new Customer { IsGold = true };
+            var result = new Product{ ListPrice = 100 }.GetPrice(customer);
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
